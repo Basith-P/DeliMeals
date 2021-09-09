37a7099bbbd590e5e53a3eb1fea42c9e167f85c1
Basith P <basithpofficial@gmail.com>
2021-09-09 22:11:08 +0530

    Set themes

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 import 'widgets/categories_screen.dart';
@@ -9,7 +11,29 @@ class DeliMealsApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'DeliMeals',
-      theme: ThemeData.light(),
+      theme: ThemeData.dark().copyWith(
+        accentColor: Colors.amber,
+        canvasColor: Color.fromRGBO(255, 254, 229, 1),
+        scaffoldBackgroundColor: Color(0xff0C0B10),
+        appBarTheme: AppBarTheme(
+          backgroundColor: Color(0xff0C0B10),
+          elevation: 0,
+        ),
+        textTheme: ThemeData.dark().textTheme.copyWith(
+              bodyText2: TextStyle(color: Color.fromRGBO(20, 51, 51, 1)),
+              bodyText1: TextStyle(color: Color.fromRGBO(20, 51, 51, 1)),
+              headline6: TextStyle(
+                fontSize: 20,
+                fontFamily: 'RobotoCondensed',
+                // fontWeight: FontWeight.normal,
+              ),
+              headline1: TextStyle(
+                fontFamily: 'RobotoCondensed',
+                fontSize: 32,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+      ),
       debugShowCheckedModeBanner: false,
       home: CategoriesPage(),
     );
--- lib/widgets/categories_screen.dart
+++ lib/widgets/categories_screen.dart
@@ -8,7 +8,10 @@ class CategoriesPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Deli Meals'),
+        title: Text(
+          'Deli Meals',
+          style: Theme.of(context).textTheme.headline1,
+        ),
         centerTitle: true,
       ),
       body: SafeArea(
