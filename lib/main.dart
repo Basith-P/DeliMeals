7636058743393c3272b5605bd5576f504fea1818
Basith P <basithpofficial@gmail.com>
2021-09-09 20:45:10 +0530

    Gridview basic design

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'widgets/categories_screen.dart';
+
 void main() => runApp(DeliMealsApp());
 
 class DeliMealsApp extends StatelessWidget {
@@ -7,31 +9,9 @@ class DeliMealsApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'DeliMeals',
-      theme: ThemeData.dark().copyWith(
-        scaffoldBackgroundColor: Color(0xff292D32),
-      ),
+      theme: ThemeData.light(),
       debugShowCheckedModeBanner: false,
-      home: MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('DeliMeals'),
-        centerTitle: true,
-      ),
-      body: Center(
-        child: Text('Navigation Time!'),
-      ),
+      home: CategoriesPage(),
     );
   }
 }
--- lib/widgets/categories_screen.dart
+++ lib/widgets/categories_screen.dart
@@ -6,19 +6,28 @@ import 'category_item.dart';
 class CategoriesPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return GridView(
-      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-        crossAxisCount: 2,
-        childAspectRatio: 2 / 3,
-        mainAxisSpacing: 20,
-        crossAxisSpacing: 20,
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Deli Meals'),
+        centerTitle: true,
       ),
-      children: DUMMY_CATEGORIES
-          .map((e) => CategoryItem(
-                title: e.title,
-                color: e.color,
-              ))
-          .toList(),
+      body: SafeArea(
+        child: GridView(
+          padding: const EdgeInsets.all(15),
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+            childAspectRatio: 3 / 2,
+            mainAxisSpacing: 15,
+            crossAxisSpacing: 15,
+          ),
+          children: DUMMY_CATEGORIES
+              .map((e) => CategoryItem(
+                    title: e.title,
+                    color: e.color,
+                  ))
+              .toList(),
+        ),
+      ),
     );
   }
 }
